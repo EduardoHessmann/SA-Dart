b13f59d0fb168f73649bb8808ecd0aab9d3c966a
Eduardo Hessmann <eduardo.h.rohleder@gmail.com>
2025-02-18 20:35:09 -0300

    Documentação do código

--- lib/interface/menu.dart
+++ lib/interface/menu.dart
@@ -2,9 +2,11 @@ import 'dart:io';
 import 'package:todo_list_dart/modelos/tarefa.dart';
 
 class Menu {
-  int opcao = 0;
+  int opcao = 0; // Variável para guardar a opção que o usuário escolher;
 
+  //Método para mostrar as opções;
   void mostrarOpcoes() {
+    //Laço While com a condeção que a opção escolhida nao pode ser 5;
     while (opcao != 5) {
       print("\n================ MENU ================");
       print("1- Adicionar Tarefa");
@@ -16,12 +18,12 @@ class Menu {
       String? opcaoCoverter = stdin.readLineSync();
       opcao = int.parse(opcaoCoverter!);
 
-      Tarefa gerenciadorTarefa = Tarefa.vazio();
+      Tarefa gerenciadorTarefa = Tarefa.vazio(); //Instância vazia de tarefa para acessar os métodos da tarefa;
 
+      //Switch Case para cada opção que o usuário escolher;
       switch (opcao) {
         case 1:
           gerenciadorTarefa.adicionarTarefa();
-
           break;
 
         case 2:
@@ -39,7 +41,6 @@ class Menu {
         case 5:
           print("Programa Finalizado!");
           opcao = 5;
-
           break;
         default:
       }
