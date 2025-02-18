f48e80d02b2edb4f1a318262ea93359094217a43
Eduardo Hessmann <eduardo.h.rohleder@gmail.com>
2025-02-18 20:31:06 -0300

    Documentação do código

--- lib/modelos/tarefa.dart
+++ lib/modelos/tarefa.dart
@@ -1,20 +1,22 @@
 import 'dart:io';
 
 class Tarefa {
-  static int contadorIdTarefa = 1;
+  static int contadorIdTarefa = 1; // variável estática para guardar o numero de tarefas criadas, e atribuir um ID auto incrementável à tarefa;
 
-  late int _idTarefa = 0;
-  late String _descTarefa = " ";
-  late String _statusTarefa = " ";
+  late int _idTarefa = 0; // Variável id da tarefa;
+  late String _descTarefa = " "; // Variável de descrição da tarefa;
+  late String _statusTarefa = " "; // Variável status da tarefa;
 
-  static List<Tarefa> tarefas = [];
+  static List<Tarefa> tarefas = []; // Lista para guardar as tarefas;
 
-  Tarefa(this._descTarefa, this._statusTarefa){
-      _idTarefa = contadorIdTarefa++;
+  Tarefa(this._descTarefa, this._statusTarefa){  //Construtor da tarefa
+      _idTarefa = contadorIdTarefa++; // O id está sendo incrementado automaticamente pelo contador de IDs;
   }
 
-  Tarefa.vazio();
+  Tarefa.vazio(); // Construtor vazio para a tarefa;
 
+  //Área para getters e setters dos atributos da tarefa;
+  
   int get idTarefa => _idTarefa;
   set idTarefa(int value) => _idTarefa = value;
 
@@ -24,11 +26,12 @@ class Tarefa {
   String get statusTarefa => _statusTarefa;
   set statusTarefa(String value) => _statusTarefa = value;
 
+  //Método de adicionar tarefa;
   void adicionarTarefa() {
     print("Informe a descrição da tarefa:");
     String? desc = stdin.readLineSync();
 
-    _idTarefa++;
+    contadorIdTarefa++;
 
     Tarefa tarefa = Tarefa(desc!, "EM ANDAMENTO");
 
@@ -37,6 +40,7 @@ class Tarefa {
     print("\nTarefa adicionada com seucesso!");
   }
 
+  //Método para listar as tarefas;
   void listarTarefas() {
     print("\n============== Listagem ==============");
     for (var tarefa in tarefas) {
@@ -47,6 +51,7 @@ class Tarefa {
     }
   }
 
+  //Método para concluir uma tarefa
   void concluirTarefa() {
     print("Informe o ID da tarefa que deseja concluir: ");
     String? idInput = stdin.readLineSync();
@@ -59,7 +64,7 @@ class Tarefa {
     }
     print("\nTarefa concluída com Sucesso!");
   }
-
+// Método para remover uma tarefa;
   void removerTarefa() {
     print("Informe o ID da tarefa que deseja remover: ");
     String? idInput = stdin.readLineSync();
